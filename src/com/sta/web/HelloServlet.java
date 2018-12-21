bb44fbf2fd5c9c65d617ff0f02cb8ed169e0f43a
space <906560643@qq.com>
2018-12-21 13:02:43 +0800

    Initial commit

---
+++ src/com/sta/web/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.sta.web;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+       // Gson gson = new Gson();
+        response.getWriter().write("hello com.sta.web");
+        System.out.println("hello servlet");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
